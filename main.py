40a694d026ac0c0f2e8b174f800040a1a9525a79
Pablo Rey <pablo.rey.marino@rai.usc.es>
2024-10-03 10:05:06 +0200

    Agregadas nuevas funciones al main

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ from lib import cuadrado
 
 print("Proyecto figuras")
 
+print(cuadrado.get_identificador())
+
 lado = 4
-print(f"El area de un cuadrado de lado {lado} es: {cuadrado.get_area(lado)}")
+print(f"El area de un {cuadrado.get_identificador()} de lado {lado} es: 
+      {cuadrado.get_area(lado)} y el perímetro es {cuadrado.get_perimetro(lado)}")
